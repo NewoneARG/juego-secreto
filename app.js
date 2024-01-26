5e72bfbcd8575083657bf0f6abb6ce9a9134c0af
Maximiliano German Carranza <nxnewone@gmail.com>
2024-01-26 19:28:19 -0300

    Version Final Juego numero secreto

---
+++ app.js
@@ -0,0 +1,69 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 3;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+
+  console.log(intentos);
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento(
+      "p",`Acertaste el numero en ${intentos} ${intentos === 1 ? "vez" : "veces"}!`);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento("p", "El numero secreto es menor");
+    } else {
+      asignarTextoElemento("p", "El numero secreto es mayor");
+    }
+    intentos++;
+    limpiarCaja();
+  }
+  return;
+}
+
+function limpiarCaja() {
+  document.querySelector("#valorUsuario").value = "";
+}
+
+function generarNumeroSecreto() {
+  let numeroGenerado = Math.floor(Math.random() * 10) + 1;
+  console.log(numeroGenerado);
+  console.log();
+  if (listaNumerosSorteados.length == numeroMaximo) {
+    asignarTextoElemento("p", "Ya se sortearon todos los numeros posibles");
+  } else {
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+      return generarNumeroSecreto();
+    } else {
+      listaNumerosSorteados.push(numeroGenerado);
+      return numeroGenerado;
+    }
+  }
+}
+
+function condicionesIniciales() {
+  asignarTextoElemento("h1", "Juego del número secreto!");
+  asignarTextoElemento("p", `Indica un número del 1 al 10`);
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  limpiarCaja();
+
+  condicionesIniciales();
+
+  document.querySelector("#reiniciar").setAttribute("disabled", "true");
+}
+
+condicionesIniciales();
